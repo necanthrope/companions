0bf005feee0d8aac74bb64ee70d5a3e8e7beaa8b
Jeremy Tidwell <necanthrope@users.noreply.github.com>
2023-07-24 14:41:41 -0700

    playbook move support

---
+++ module/helpers/playbooks/whizData.mjs
@@ -0,0 +1,28 @@
+export const WhizData = {};
+
+WhizData.moves = {
+    "playbook": {
+        "": {
+            "title": "",
+            "description": "",
+            "type": "",
+            "stat": [],
+            "fullHit": "",
+            "partialHit": "",
+            "miss": "",
+            "options": "",
+        },
+    },
+    "romance": {
+        "whizRomance": {
+            "title": "The Whiz's Romance Move",
+            "description": "",
+            "type": "",
+            "stat": [],
+            "fullHit": "",
+            "partialHit": "",
+            "miss": "",
+            "options": "",
+        }
+    }
+};
---
+++ module/helpers/playbooks/witnessData.mjs
@@ -0,0 +1,28 @@
+export const WitnessData = {};
+
+WitnessData.moves = {
+    "playbook": {
+        "": {
+            "title": "",
+            "description": "",
+            "type": "",
+            "stat": [],
+            "fullHit": "",
+            "partialHit": "",
+            "miss": "",
+            "options": "",
+        },
+    },
+    "romance": {
+        "witnessRomance": {
+            "title": "The Witness's Romance Move",
+            "description": "",
+            "type": "",
+            "stat": [],
+            "fullHit": "",
+            "partialHit": "",
+            "miss": "",
+            "options": "",
+        }
+    }
+};
